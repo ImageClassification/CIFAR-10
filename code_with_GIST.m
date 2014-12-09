9da602155d56e27046251f3a0c773522cd08c378
kavyasrinet <kavyasrinet@Kavyas-MacBook-Air.local>
2014-12-09 10:31:34 -0500

    GIST and SVM

--- code_with_GIST.m
+++ code_with_GIST.m
@@ -68,112 +68,58 @@ function runClassifier(dataset, opt)
         end
         K = max(y_train(:));
     end
-%     Data = [X_train;X_test] ;
-     
- %    [rows_train,cols_train] = size(X_train)
- %    [rows_test, cols_test] = size(X_test)
-     
-    % train and test classifier
-%     [coeff,scores,latent] = pca(Data,'NumComponents',1000);
-%    train_mat = scores(1:rows_train,:);
-%    rows_train
-%    size(train_mat)
-%    test_mat = scores(rows_train+1:rows_train+rows_test,:);
-%     rows_test
-%    size(test_mat)
-%    mdl = fitcknn(train_mat,y_train,'NumNeighbors',3,'NSMethod','exhaustive','Distance','minkowski','Standardize',1);
-%    label = predict(mdl,Xnew);
-%    size(label);
-%     label
-
-%reshape to imread
 
 
 
     % GIST Parameters:
-% clear param
-% param.imageSize = [256 256]; % set a normalized image size
-% param.orientationsPerScale = [8 8 8 8]; % number of orientations per scale (from HF to LF)
-% param.numberBlocks = 4;
-% param.fc_prefilt = 4;
-% 
-% [Nimages,cols] = size(X_train);
-% % Pre-allocate gist:
-% Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
-% gist = zeros([Nimages Nfeatures]); 
-% 
-% % Load first image and compute gist:
-%  img = reshape(X_train(1,:),32,32,3);
-%  [gist(1, :), param] = LMgist(img, '', param); % first call
-% % Loop:
-% for i = 2:Nimages
-%     img = reshape(X_train(i,:),32,32,3);
-%     sprintf('%s %d','here train',i)
-%   % img = X_train(i,:);
-%    gist(i, :) = LMgist(img, '', param); % the next calls will be faster
-% end
-% 
-% size(gist)
-% save('./gist_train.mat','gist')
-% [Nimages_test,cols] = size(X_test);
-% Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
-% gist_test = zeros([Nimages_test Nfeatures]); 
-% 
-% % Load first image and compute gist:
-% img_test = reshape(X_test(i,:),32,32,3);
-% [gist_test(1, :), param] = LMgist(img_test, '', param); % first call
-% % Loop:
-% for i = 2:Nimages_test
-%     img_test = reshape(X_test(i,:),32,32,3);
-%     sprintf('%s here test %d','here test',i)
-%   % img_test = X_test(i,:);
-%   gist_test(i,:) = LMgist(img_test, '', param);
-%   
-% %   gist_test = [gist_test;gist_test(i,:)]; % the next calls will be faster
-% end
-% save('./gist_test.mat','gist_test')
-% size(gist_test)
-%     params = trainClassifier(gist(1:end,:), y_train(1:end), opt);
-%     preds = predictClassifier(params, gist);
-%     fprintf('Train Accuracy = %.2f%%\n', 100*mean(preds(:) == y_train(:)));
-%     
-%     preds = predictClassifier(params, gist_test);
-%     fprintf('Test Accuracy = %.2f%%\n', 100*mean(preds(:) == y_test(:)));
-%     
+ clear param
+ param.imageSize = [256 256]; % set a normalized image size
+ param.orientationsPerScale = [8 8 8 8]; % number of orientations per scale (from HF to LF)
+ param.numberBlocks = 4;
+ param.fc_prefilt = 4;
+ 
+ [Nimages,cols] = size(X_train);
+ % Pre-allocate gist:
+ Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
+ gist = zeros([Nimages Nfeatures]); 
+ 
+ % Load first image and compute gist:
+  img = reshape(X_train(1,:),32,32,3);
+  [gist(1, :), param] = LMgist(img, '', param); % first call
+ % Loop:
+ for i = 2:Nimages
+     img = reshape(X_train(i,:),32,32,3);
+    
+    gist(i, :) = LMgist(img, '', param); % the next calls will be faster
+ end
+ 
+
+ save('./gist_train.mat','gist')
+ [Nimages_test,cols] = size(X_test);
+ Nfeatures = sum(param.orientationsPerScale)*param.numberBlocks^2;
+ gist_test = zeros([Nimages_test Nfeatures]); 
+ 
+ % Load first image and compute gist:
+ img_test = reshape(X_test(i,:),32,32,3);
+ [gist_test(1, :), param] = LMgist(img_test, '', param); % first call
+ % Loop:
+ for i = 2:Nimages_test
+     img_test = reshape(X_test(i,:),32,32,3);
+     gist_test(i,:) = LMgist(img_test, '', param);
+
+ end
+ save('./gist_test.mat','gist_test')
+    
    
 load('./gist_test.mat')
 load('./gist_train.mat')
-disp('model oaded')
-size(gist)
-size(gist_test)
+
 model_pca = svmtrain(y_train,gist,'-c 100 -g 0.9 -t 2');
-disp('train done')
+
 [predict_label_L, accuracy_testpca1, dec_values_L] = svmpredict(y_test, gist_test, model_pca);
 % write the data out to a file that can be read by Kaggle.
     writeLabels('my_labels.csv', predict_label_L);
     
-    % plot the decision boundary or return if not plottable.
-%     if n ~= 2 || ~isfield(opt, 'display') || ~opt.display, return; end;
-%     
-%     xmin = min(gist(:, 1));
-%     xmax = max(gist(:, 1)); 100 , 0.06
-%     ymin = min(gist(:, 2));
-%     ymax = max(gist(:, 2));
-%     
-%     if K <= 6 % make pretty colors for scatter plot for up to 6 classes.
-%         colors = [0 0 1; 1 0 0; 0 1 0; 0 1 1; 1 1 0; 1 0 1];
-%         light_colors = colors + .8;
-%         light_colors(light_colors > 1) = 1;
-%         colormap(light_colors(1:K, :));
-%     end
-%     
-%    % hold on;
-%     plotBoundary(@(x) predictClassifier(params, x),...
-%                  xmin, xmax, ymin, ymax);
-%     if K <= 6, cy = colors(y_train,:); else cy = y_train; end;
-%     scatter(gist(:, 1), gist(:, 2), [], cy, 'filled');
-%     axis equal tight
-%     hold off;
 end
 
 function [X, y] = generateData(m, centers)
